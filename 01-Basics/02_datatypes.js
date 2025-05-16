db22d570a785f3a41472cc11daa1e30c232cf742
Eshwar K S <eshwarks1234@gmail.com>
2025-05-16 20:04:44 +0530

    Datatypes

---
+++ 01-Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as in strict mode(newer version of JS)
+
+//alert(3+3) // alert(3+3) // alert is a browser function
+//code shold be readable
+//console.log(3+3) // console.log is a browser function
+
+let firstName = "Esh" //string
+let age = 22 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+let nullVariable = null //null
+
+// number => 2to power 53
+// BigInt => 2to power 53
+// string => "Esh" => 'Esh' => `Esh`
+// boolean => true/false
+// undefined => let state; // no value assigned
+// null => let nullVariable = null // no value assigned // standalone value
+// symbol => unique value
+// BigInt => 1234567890123456789012345678901234567890n
+
+//Object => collection of key value pairs
+
+console.log(typeof firstName); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
