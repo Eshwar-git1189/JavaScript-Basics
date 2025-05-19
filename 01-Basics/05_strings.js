271349dd3ef2dbbb9c7388d1dde34576863179b7
Eshwar K S <eshwarks1234@gmail.com>
2025-05-19 17:19:16 +0530

    Strings

---
+++ 01-Basics/05_strings.js
@@ -0,0 +1,104 @@
+const name = "Eshwar";
+const repoCount = 10;
+
+//console.log(name + repoCount + "Value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Eshwar");
+// console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(2, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-4, 4);
+console.log(anotherString);
+
+const newString1 = "   Eshwar   ";
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://eshwar.com/eshwar%20ks";
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("eshwar"));
+
+console.log(url.split("/"));
+
+
+
+
+// const gameName = new String('Eshwar')
+// gameName
+// String {'Eshwar'}
+// 0: "E"
+// 1: "s"
+// 2: "h"
+// 3: "w"
+// 4: "a"
+// 5: "r"
+// length: 6
+// [[Prototype]]: String
+// anchor: ƒ anchor()
+// at: ƒ at()
+// big: ƒ big()
+// blink: ƒ blink()
+// bold: ƒ bold()
+// charAt: ƒ charAt()
+// charCodeAt: ƒ charCodeAt()
+// codePointAt: ƒ codePointAt()
+// concat: ƒ concat()
+// constructor: ƒ String()
+// endsWith: ƒ endsWith()
+// fixed: ƒ fixed()
+// fontcolor: ƒ fontcolor()
+// fontsize: ƒ fontsize()
+// includes: ƒ includes()
+// indexOf: ƒ indexOf()
+// isWellFormed: ƒ isWellFormed()
+// italics: ƒ italics()
+// lastIndexOf: ƒ lastIndexOf()
+// length: 0link: ƒ link()
+// localeCompare: ƒ localeCompare()
+// match: ƒ match()
+// matchAll: ƒ matchAll()
+// normalize: ƒ normalize()
+// padEnd: ƒ padEnd()
+// padStart: ƒ padStart()
+// repeat: ƒ repeat()
+// replace: ƒ replace()
+// replaceAll: ƒ replaceAll()
+// search: ƒ search()
+// slice: ƒ slice()
+// small: ƒ small()
+// split: ƒ split()
+// startsWith: ƒ startsWith()
+// strike: ƒ strike()
+// sub: ƒ sub()
+// substr: ƒ substr()
+// substring: ƒ substring()
+// sup: ƒ sup()
+// toLocaleLowerCase: ƒ toLocaleLowerCase()
+// toLocaleUpperCase: ƒ toLocaleUpperCase()
+// toLowerCase: ƒ toLowerCase()
+// toString: ƒ toString()
+// toUpperCase: ƒ toUpperCase()
+// toWellFormed: ƒ toWellFormed()
+// trim: ƒ trim()
+// trimEnd: ƒ trimEnd()
+// trimLeft: ƒ trimStart()
+// trimRight: ƒ trimEnd()
+// trimStart: ƒ trimStart()
+// valueOf: ƒ valueOf()
+// Symbol(Symbol.iterator): ƒ [Symbol.iterator]()
+// [[Prototype]]: Object
+// [[PrimitiveValue]]: ""
+// [[PrimitiveValue]]: "Eshwar"
