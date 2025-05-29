67172f796b5c882d0bbe97e9e70d1808132227b1
Eshwar K S <eshwarks1234@gmail.com>
2025-05-29 23:02:25 +0530

    03_Objects started

---
+++ 02-Basics/03_objects.js
@@ -0,0 +1,14 @@
+//Objects in depth in JavaScript 
+
+//Objects are a collection of key-value pairs
+const person = {   
+    name: 'John',
+    age: 30,
+    isEmployed: true,
+    hobbies: ['reading', 'gaming', 'hiking'],
+    address: {
+        street: '123 Main St',
+        city: 'Anytown',
+        country: 'USA'
+    }
+};
