8f3d774d918c5ba767ce36449f57d0296b6c1975
Eshwar K S <eshwarks1234@gmail.com>
2025-06-02 23:16:34 +0530

    Objects

--- 02-Basics/03_objects.js
+++ 02-Basics/03_objects.js
@@ -1,5 +1,7 @@
 //Objects in depth in JavaScript 
 
+
+
 //Objects are a collection of key-value pairs
 const person = {   
     name: 'John',
@@ -12,3 +14,59 @@ const person = {
         country: 'USA'
     }
 };
+// Singleton object
+const singleton = {
+    name: 'Singleton',
+    value: 42,
+    getValue: function() {
+        return this.value;
+    }
+};
+
+// object literals
+
+const mySym = Symbol("key1")
+const JsUser = {
+    name:"Jane Doe",
+    "full name":"Jane Doe Simon", // Using quotes for property names with spaces
+    [mySym]:"myKey1",
+    age: 25,
+    location: "New York",
+    email:"esmvimi@vmmro.cum",
+    isLoggedIn: true,
+    lastLoginDays:["Monday", "Wednesday", "Friday"]
+}
+
+// console.log(JsUser.name); // Accessing property using dot notation
+// console.log(JsUser['email']);//Better to use this format // Accessing property using bracket notation
+// console.log(JsUser["full name"]);// Only this way to print 
+// console.log(JsUser[mySym]);
+
+// //console.log(mySym["myKey1"]);
+// console.log(typeof JsUser[mySym]);
+
+
+JsUser.email = "jngv@nic.vig"
+//Object.freeze(JsUser)
+JsUser.email = "kkkdd33k"
+//console.log(JsUser);
+
+
+
+JsUser.greeting = function(){
+    console.log("Hello js user!!! ");
+    
+}
+console.log(JsUser.greeting());
+
+
+JsUser.greetingTwo= function(){
+    console.log(`Hello js user!!!,${this.name} `);
+    
+}
+console.log(JsUser.greetingTwo());
+
+
+
+
+
